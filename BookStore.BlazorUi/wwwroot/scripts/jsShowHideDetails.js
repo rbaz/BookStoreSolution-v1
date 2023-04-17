bb485875f823a14bdc00d69a924843cbf5d49875
rBaz <bouazzarochdi@gmail.com>
2023-04-17 01:46:13 -0400

    Fix the double showed detail and remove line after twice click on plus image

--- BookStore.BlazorUi/wwwroot/scripts/jsShowHideDetails.js
+++ BookStore.BlazorUi/wwwroot/scripts/jsShowHideDetails.js
@@ -1,18 +1,17 @@
 ﻿// Assign Click event to Plus Image.
 $(document).on("click", "img[src*='plus.png']", function () {
-    /*debugger; // pause execution here    */
-    var nextElem = $(this).next();
-    /*if (nextElem.length) {*/
-        /*$(this).closest("tr").after("<tr><td></td><td colspan='999'>" + nextElem.html() + "</td></tr>");*/
-        $(this).attr("src", "/images/minus.png");
-    //}
+    var detailRow = $(this).closest("tr").next(".details-row");
+    detailRow.toggle();
+    $(this).attr("src", "/images/minus.png");
 });
 
 // Assign Click event to Minus Image.
 $(document).on("click", "img[src*='minus.png']", function () {
     $(this).attr("src", "/images/plus.png");
-    $(this).closest("tr").next().remove();
+    var detailRow = $(this).closest("tr").next(".details-row");
+    detailRow.hide();  // hide the detail row instead of removing it
 });
+
 
 function toggleElementVisibility(elementId) {
     let element = document.getElementById(elementId);
